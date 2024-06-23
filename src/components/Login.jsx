62a652a6f7689644be35ded0fb0a359e2969d289
Atharva <atharva@192.168.1.3>
2024-06-23 22:03:43 +0530

    Logout and inital token

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -3,21 +3,15 @@ import { useAuth0 } from '@auth0/auth0-react';
 import Navbar from './Navbar'; // Assuming you have a Navbar component
 import Chat from './Chat'; // Assuming you have a Chat component
 import { FaUserCircle } from 'react-icons/fa'; // Assuming you are using react-icons for the user icon
-// import { useNavigate } from 'react-router-dom';
 
 const LoginPage = () => {
-
-  const { loginWithRedirect, logout, getAccessTokenSilently } = useAuth0();
+  const { loginWithRedirect, logout, isAuthenticated, getAccessTokenSilently } = useAuth0();
   const [showLogout, setShowLogout] = useState(false);
-
-  // const noToken = localStorage.getItem('myItem') === null
-
-  const [isAuth, setisAuth] = useState(useAuth0())
 
   useEffect(() => {
     const getToken = async () => {
       try {
-        if (isAuth) {
+        if (isAuthenticated) {
           const token = await getAccessTokenSilently();
           localStorage.setItem('authToken', token); // Store token in localStorage
         }
@@ -25,23 +19,22 @@ const LoginPage = () => {
         console.error("Error getting token: ", error);
       }
     };
+
     getToken();
-  }, [isAuth, getAccessTokenSilently]);
+  }, [isAuthenticated, getAccessTokenSilently]);
 
   const handleLogout = () => {
     logout({ returnTo: window.location.origin });
     localStorage.removeItem('authToken'); // Remove token from localStorage
-    setisAuth(false)
-    // navigate('/logout') 
   };
 
   const toggleLogout = () => {
     setShowLogout(!showLogout);
   };
 
-  return (!isAuth) ? (
+  return !isAuthenticated ? (
     <div className="flex h-screen items-center justify-center bg-gray-100">
-      <div className="bg-white p-8 rounded-lg shadow-md w-96 text-center"> 
+      <div className="bg-white p-8 rounded-lg shadow-md w-96 text-center">
         <h1 className="text-3xl text-blue-600 font-bold mb-6">ASSIST</h1>
         <h2 className="text-2xl font-bold mb-6">Login to Alliance Platform</h2>
         <div className="mb-4">
